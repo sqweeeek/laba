e39655581cd68bcf2660e2fee9b89b851593f839
sqweeeek <130255526+sqweeeek@users.noreply.github.com>
2023-10-30 05:03:39 +0300

    Create 7.1.py

---
+++ задание 7/7.1.py
@@ -0,0 +1,14 @@
+class Employee:
+
+    name = None
+    salary = None
+
+    def showName(self):
+        return f'name: {self.name}'
+
+    def showSalary(self):
+        return f'salary: {self.salary}'
+
+emp = Employee()
+emp.name = 'Ksyusha'
+emp.slary = 150000
